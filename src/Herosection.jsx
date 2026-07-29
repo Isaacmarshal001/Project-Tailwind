3aaab077f60b9d47fd083190ec0eb081581924ab
Isaac <falanamarshal@gmail.com>
2026-07-29 18:02:58 +0100

    updated2

---
+++ src/Herosection.jsx
@@ -0,0 +1,37 @@
+
+
+function Herosection() {
+    return (
+        <div>
+            <div>
+                <div>
+                    <img src="#" alt="logo" />
+                </div>
+                <div>
+                    <a href="#"></a>
+                    <a href="#"></a>
+                    <a href="#"></a>
+                    <a href="#"></a>
+                    <a href="#"></a>
+                </div>
+                <div>
+                    <p>Search Bar</p>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <p>Announcing our next round of funding. <a href="#">Read more</a> </p>
+                    <h1>Data to enrich your online bussiness</h1>
+                    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil, quibusdam nostrum labore distinctio nobis modi iusto pariatur veniam, quam vero facilis adipisci ipsa dolorem quisquam est suscipit cupiditate assumenda! Obcaecati! </p>
+                    <div>
+                        <button>Get started</button>
+                        <button>Learn more -</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Herosection;
