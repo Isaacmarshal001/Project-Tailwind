f76a326b3ed20af30fb3169065094b0e0e4ffca3
Isaac <falanamarshal@gmail.com>
2026-08-15 21:40:39 +0100

    .

--- src/Newslettersections1.jsx
+++ src/Newslettersections1.jsx
@@ -2,17 +2,17 @@ import { Link } from "react-router"
 
 function Newslettersections1() {
     return (
-        <div className="bg-black min-h-screen min-w-screen">
-            <div className="mx-auto bg-violet-600 flex items-center justify-center gap-7 p-7 border border-600/50 rounded-xlw-[95]">
-                <div className="flex items-center text-left w-full">
+        <div className="bg-black w-full h-full">
+            <div className="mx-auto bg-violet-600 flex items-center justify-center gap-7 border border-600/50 rounded-xl w-[95%]">
+                <div className="flex items-center text-left w-[60%]">
                     <h1 className="text-4xl font-bold">Want product news and updates? Sign up for our newsletter.</h1>
                 </div>
                 <div className="flex flex-col items-center justify-center gap-4">
                     <div className="flex items-center justify-center gap-4">
                         <input type="text" />
-                        <button>Subscribe</button>
+                        <button className="bg-purple-600 py-2 px-4 rounded-gray-200/50 rounded-xl">Subscribe</button>
                     </div>
-                    <p>We care about your data. Read our privacy policy.</p>
+                    <p className="text-left">We care about your data. Read our privacy policy.</p>
                 </div>
             </div>
         </div>
